dfc2b7654f7862e32546e5cb7373e385a0a3cc57
Linus Hernvall <linus.hernvall@gmail.com>
2022-11-16 08:00:07 +0100

    Added some paths.

---
+++ javascript/main.js
@@ -0,0 +1,125 @@
+window.addEventListener("DOMContentLoaded", main);
+
+/** @type {HTMLParagraphElement} shows a p-tag with the current scene */
+let storyText;
+/** @type {HTMLButtonElement} gives the user the first option to choose from in every scene  */
+let buttonOption1;
+/** @type {HTMLButtonElement} gives the user the second option to choose from in every scene  */
+let buttonOption2;
+
+/** gets the HTML-elements and the start scene */
+function main() {
+    getHtmlElements();
+    getStartScene();
+}
+
+/** shows the HTML-elements that the function contains of */
+function getHtmlElements() {
+    storyText = document.getElementById('text-story');
+    buttonOption1 = document.getElementById('btn-option1');
+    buttonOption2 = document.getElementById('btn-option2');
+}
+
+/** gets the first scene - the user's first path selection   */
+function getStartScene() {
+    storyText.textContent = "This is the first day of your future music career. You're in the music store and you can't \
+    decide if you want to start to play guitar or drums.";
+
+    buttonOption1.textContent = 'Guitar';
+    buttonOption1.addEventListener('click', getGuitar);
+    
+    buttonOption2.textContent = 'Drums';
+    buttonOption2.addEventListener('click', getDrums);
+}
+
+/** gets the scene if the user chooses "Guitar" */
+function getGuitar() {
+    storyText.textContent = "Some months have passed since you got your guitar. It's starting to get pretty hard and it \
+    demands further practicing for you to get any better. It would be easier and much more fun to play \
+    video games at this moment.";
+
+    buttonOption1.textContent = 'Practice guitar';
+    buttonOption1.addEventListener('click', getGuitarPractice);
+    
+    buttonOption2.textContent = 'Play video games';
+    buttonOption2.addEventListener('click', getGuitarGames);
+}
+
+/** gets the scene if the user chooses "Drums" */
+function getDrums() {
+    storyText.textContent = "Some months have passed since you got your drums. It's starting to get pretty hard and it \
+    demands further practicing for you to get any better. It would be easier and much more fun to play \
+    video games at this moment.";
+
+    buttonOption1.textContent = 'Practice drums';
+    buttonOption1.addEventListener('click', getDrumsPractice);
+    
+    buttonOption2.textContent = 'Play video games';
+    buttonOption2.addEventListener('click', getDrumsGames);
+}
+
+/** gets the scene if the user chooses "Practice guitar" */
+function getGuitarPractice() {
+    storyText.textContent = "It's time for your first gig. Fortunately you've been practicing a lot, so you perform at your \
+    highest level. An agent had his eyes on you the whole evening and he really liked what he saw. He \
+    approaches you with an offer to join a much bigger band who's on their way up to success.";
+
+    buttonOption1.textContent = 'Accept the offer';
+    buttonOption1.addEventListener('click', getAcceptAgent);
+    
+    buttonOption2.textContent = 'Decline the offer';
+    buttonOption2.addEventListener('click', getDeclineAgent);
+}
+
+/** gets the scene if the user chooses "Play video games" */
+function getGuitarGames() {
+    storyText.textContent = "It's time for your first gig. You really should have spent more time on practicing guitar and less \
+    on your video games. You mess up completely and the gig is a total fiasco. After this gig you put your \
+    guitar away and end your dreams of becoming a rockstar.";
+
+    buttonOption1.textContent = "Try again - It's a long way to the top if you wanna Rock 'N' Roll!";
+    buttonOption1.addEventListener('click', getStartScene);
+}
+
+/** gets the scene if the user chooses "Practice drums" */
+function getDrumsPractice() {
+    storyText.textContent = "It's time for your first gig. Fortunately you've been practicing a lot, so you perform at your \
+    highest level. After the gig an agent approaches the band, he's on the for an new guitarist. \
+    The lead guitarist of your band accepts his offer and quits your band on the spot. This leaves you \
+    completely broken and you soon decides to quit playing drums";
+
+    buttonOption1.textContent = "Try again - It's a long way to the top if you wanna Rock 'N' Roll!";
+    buttonOption1.addEventListener('click', getStartScene);
+}
+
+/** gets the scene if the user chooses "Play video games" */
+function getDrumsGames() {
+    storyText.textContent = "It's time for your first gig. You really should have spent more time on practicing drums and less \
+    on your video games. You mess up completely and the gig is a total fiasco. After this gig you put your \
+    drums away and end your dreams of becoming a rockstar.";
+
+    buttonOption1.textContent = "Try again - It's a long way to the top if you wanna Rock 'N' Roll!";
+    buttonOption1.addEventListener('click', getStartScene);
+}
+
+/** gets the scene if the user chooses "Accept the offer" */
+function getAcceptAgent() {
+    storyText.textContent = "Shortley after the deal you get to meet the band. You're really excited and have already started \
+    to write two new songs. You're not sure which of the songs you're going to put the most effort into.";
+
+    buttonOption1.textContent = 'One More Minute';
+    buttonOption1.addEventListener('click', getOneMoreMinute);
+    
+    buttonOption2.textContent = 'Heartless';
+    buttonOption2.addEventListener('click', getHeartless);
+}
+
+/** gets the scene if the user chooses "Decline the offer" */
+function getDeclineAgent() {
+    storyText.textContent = "You keep on struggling with your band, but you never seems to get the oppertunity to take your music \
+    to the next level. The time goes by and your interest in music fades away.";
+
+    buttonOption1.textContent = "Try again - It's a long way to the top if you wanna Rock 'N' Roll!";
+    buttonOption1.addEventListener('click', getStartScene);
+}
+
