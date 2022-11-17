41481097465aa3af1868678befd9c247f6f24e9f
Linus Hernvall <linus.hernvall@gmail.com>
2022-11-17 16:04:13 +0100

    Changed comments.

--- javascript/main.js
+++ javascript/main.js
@@ -13,13 +13,13 @@ let buttonOption2;
 /** @type {HTMLImageElement} sets the scene image */
 let displayImage;
 
-/** gets the HTML-elements and the start scene */
+/** Gets the HTML-elements and the start scene */
 function main() {
     getHtmlElements();
     getStartScene();
 }
 
-/** shows the HTML-elements that the function contains of */
+/** Shows the HTML-elements that the function contains of */
 function getHtmlElements() {
     storyText = document.getElementById('text-story');
     buttonOption1 = document.getElementById('btn-option1');
@@ -27,7 +27,7 @@ function getHtmlElements() {
     displayImage = document.getElementById ('scene-img');
 }
 
-/** gets the first scene - the user's first path selection   */
+/** Gets the first scene - the user's first path selection   */
 function getStartScene() {
     storyText.textContent = "This is the first day of your future music career. You're in the music store and you can't \
     decide if you want to start to play guitar or drums.";
@@ -44,7 +44,7 @@ function getStartScene() {
 
 // GUITAR
 
-/** gets the scene if the user chooses "Guitar" */
+/** Gets the scene if the user chooses "Guitar" */
 function getGuitar() {
     storyText.textContent = "Some months have passed since you got your guitar. It's starting to get pretty hard and it \
     demands further practicing for you to get any better. It would be easier and much more fun to play \
@@ -59,7 +59,7 @@ function getGuitar() {
     displayImage.src = "./images/guitar-practice.jpg"
 }
 
-/** gets the scene if the user chooses "Practice guitar" */
+/** Gets the scene if the user chooses "Practice guitar" */
 function getGuitarPractice() {
     storyText.textContent = "Your skills are getting better and better. You come across an ad for a band who’s\
     searching for a new band member.";
@@ -73,7 +73,7 @@ function getGuitarPractice() {
     displayImage.src = "./images/guitar.jpg"
 }
 
-/** gets the scene if the user chooses "Play video games" */
+/** Gets the scene if the user chooses "Play video games" */
 function getGuitarGames() {
     storyText.textContent = "A rockstar is not created through playing video games. You need to practice, practice, practice…";
 
@@ -85,7 +85,7 @@ function getGuitarGames() {
     displayImage.src = "./images/video-games.jpg"
 }
 
-/** gets the scene if the user chooses "Think I'll be better off solo" */
+/** Gets the scene if the user chooses "Think I'll be better off solo" */
 function getGuitarSolo() {
     storyText.textContent = "You're still getting on as a solo artist, but you'll \
     need to be a part of something bigger to be able to reach your dreams.";
@@ -98,7 +98,7 @@ function getGuitarSolo() {
     displayImage.src = "./images/guitar-solo.jpg"
 }
 
-/** gets the scene if the user chooses "Contact the band" */
+/** Gets the scene if the user chooses "Contact the band" */
 function getGuitarBand() {
     storyText.textContent = "It's time for a gig with your band. Fortunately you've been practicing a lot, so you perform at your \
     highest level. An agent had his eyes on you the whole evening and he really liked what he saw. He \
@@ -115,7 +115,7 @@ function getGuitarBand() {
 
 // DRUMS
 
-/** gets the scene if the user chooses "Drums" */
+/** Gets the scene if the user chooses "Drums" */
 function getDrums() {
     storyText.textContent = "Some months have passed since you got your drums. It's starting to get pretty hard and it \
     demands further practicing for you to get any better. It would be easier and much more fun to play \
@@ -130,7 +130,7 @@ function getDrums() {
     displayImage.src = "./images/drums.jpg"
 }
 
-/** gets the scene if the user chooses "Practice drums" */
+/** Gets the scene if the user chooses "Practice drums" */
 function getDrumsPractice() {
     storyText.textContent = "Your skills are getting better and better. You come across an ad for a band who’s\
     searching for a new band member.";
@@ -144,7 +144,7 @@ function getDrumsPractice() {
     displayImage.src = "./images/drums-practice.jpg"
 }
 
-/** gets the scene if the user chooses "Play video games" */
+/** Gets the scene if the user chooses "Play video games" */
 function getDrumsGames() {
     storyText.textContent = "A rockstar is not created through playing video games. You need to practice, practice, practice…";
 
@@ -156,7 +156,7 @@ function getDrumsGames() {
     displayImage.src = "./images/video-games.jpg"
 }
 
-/** gets the scene if the user chooses "Think I'll be better off solo" */
+/** Gets the scene if the user chooses "Think I'll be better off solo" */
 function getDrumsSolo() {
     storyText.textContent = "You're still getting on as a solo artist, but you'll \
     need to be a part of something bigger to be able to reach your dreams.";
@@ -169,7 +169,7 @@ function getDrumsSolo() {
     displayImage.src = "./images/drums-solo.jpg"
 }
 
-/** gets the scene if the user chooses "Contact the band" */
+/** Gets the scene if the user chooses "Contact the band" */
 function getDrumsBand() {
     storyText.textContent = "It's time for a gig with your band. Fortunately you've been practicing a lot, so you perform at your \
     highest level. After the gig an agent approaches the band, he's looking for a new guitarist. \
@@ -186,7 +186,7 @@ function getDrumsBand() {
 
 // FOLLOWING
 
-/** gets the scene if the user chooses "Accept the offer" */
+/** Gets the scene if the user chooses "Accept the offer" */
 function getAcceptAgent() {
     storyText.textContent = "Shortley after the deal you get to meet the band. You're really excited and have already started \
     to write two new songs. You're not sure which of the songs you're going to put the most effort into.";
@@ -200,7 +200,7 @@ function getAcceptAgent() {
     displayImage.src = "./images/guitar-song.jpg"
 }
 
-/** gets the scene if the user chooses "Decline the offer" */
+/** Gets the scene if the user chooses "Decline the offer" */
 function getDeclineAgent() {
     storyText.textContent = "You keep on struggling with your band, but you never seems to get the oppertunity to take your music \
     to the next level. The time goes by and your interest in music fades away.";
@@ -213,7 +213,7 @@ function getDeclineAgent() {
     displayImage.src = "./images/decline.jpg"
 }
 
-/** gets the scene if the user chooses "One More Minute" */
+/** Gets the scene if the user chooses "One More Minute" */
 function getOneMoreMinute() {
     storyText.textContent = "The time has come for the first tour with your new band. The gig at the first stop is a true \
     smash and some fans invites you to a wicked party after the gig.";
@@ -227,7 +227,7 @@ function getOneMoreMinute() {
     displayImage.src = "./images/big-gig.jpg"
 }
 
-/** gets the scene if the user chooses "Heartless" */
+/** Gets the scene if the user chooses "Heartless" */
 function getHeartless() {
     storyText.textContent = "The time has come for the first tour with your new band. The gig at the first stop is a true \
     smash and some fans invites you to a wicked party after the gig.";
@@ -241,7 +241,7 @@ function getHeartless() {
     displayImage.src = "./images/big-gig.jpg"
 }
 
-/** gets the scene if the user chooses "One More Minute - Party" */
+/** Gets the scene if the user chooses "One More Minute - Party" */
 function getOneMoreMinuteParty() {
     storyText.textContent = "The party is really wild. You get really drunk and makes a stupid decision to do some cocaine. \
     Unluckily the cocaine is spiked with high concentrations of fentanyl and you're dying of an overdose this night.";
@@ -254,7 +254,7 @@ function getOneMoreMinuteParty() {
     displayImage.src = "./images/party.jpg"
 }
 
-/** gets the scene if the user chooses "Heartless - Party" */
+/** Gets the scene if the user chooses "Heartless - Party" */
 function getHeartlessParty() {
     storyText.textContent = "The party is really wild. You get really drunk and makes a stupid decision to do some cocaine. \
     Unluckily the cocaine is spiked with high concentrations of fentanyl and you're dying of an overdose this night.";
@@ -267,7 +267,7 @@ function getHeartlessParty() {
     displayImage.src = "./images/party.jpg"
 }
 
-/** gets the scene if the user chooses "One More Minute - Sleep" */
+/** Gets the scene if the user chooses "One More Minute - Sleep" */
 function getOneMoreMinuteSleep() {
     storyText.textContent = "The time goes on with you and the band. Unfortunatly you never manage to write a real hit song and \
     never makes it to the top. You end up taking a regular day job and your dream of becoming a rockstar is shattered.";
@@ -280,7 +280,7 @@ function getOneMoreMinuteSleep() {
     displayImage.src = "./images/sunset.jpg"    
 }
 
-/** gets the scene if the user chooses "Heartless - Sleep" */
+/** Gets the scene if the user chooses "Heartless - Sleep" */
 function getHeartlessSleep() {
     storyText.textContent = "The time goes on with you and the band and the 'Heartless' that you wrote becomes a megahit. \
     It's topping all the big lists all over the world. The money keeps on coming and your're now able to dedicate \
